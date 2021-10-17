6f503786cd19a3503dffe465c0152b6803e0c25a
rick <1450685+LinuxSuRen@users.noreply.github.com>
2021-10-17 20:44:19 +0800

    Add script to publish over GitHub

--- go.mod
+++ go.mod
@@ -3,6 +3,8 @@ module github.com/linuxsuren/goget
 go 1.16
 
 require (
-	github.com/go-git/go-git/v5 v5.4.2 // indirect
-	github.com/spf13/cobra v1.2.1 // indirect
+	github.com/go-git/go-git/v5 v5.4.2
+	github.com/gosuri/uilive v0.0.4 // indirect
+	github.com/gosuri/uiprogress v0.0.1
+	github.com/spf13/cobra v1.2.1
 )
---
+++ pkg/ui/progress.go
@@ -0,0 +1,71 @@
+package ui
+
+import (
+	"fmt"
+	"github.com/gosuri/uiprogress"
+	"io"
+)
+
+// ProgressIndicator hold the progress of io operation
+type ProgressIndicator struct {
+	Writer io.Writer
+	Reader io.Reader
+	Title  string
+
+	// bytes.Buffer
+	Total float64
+	count float64
+	bar   *uiprogress.Bar
+}
+
+var process *uiprogress.Progress
+
+// Init set the default value for progress indicator
+func (i *ProgressIndicator) Init() {
+	// start rendering
+	if process == nil {
+		process = uiprogress.New()
+		process.Start()
+	}
+	i.bar = process.AddBar(100) // Add a new bar
+
+	// optionally, append and prepend completion and elapsed time
+	i.bar.AppendCompleted()
+	//i.bar.PrependElapsed()
+
+	if i.Title != "" {
+		i.bar.PrependFunc(func(_ *uiprogress.Bar) string {
+			return fmt.Sprintf("%s: ", i.Title)
+		})
+	}
+}
+
+// Close shutdowns the ui process
+func (i ProgressIndicator) Close() {
+	if process != nil {
+		process.Stop()
+		process = nil
+	}
+}
+
+// Write writes the progress
+func (i *ProgressIndicator) Write(p []byte) (n int, err error) {
+	n, err = i.Writer.Write(p)
+	i.setBar(n)
+	return
+}
+
+// Read reads the progress
+func (i *ProgressIndicator) Read(p []byte) (n int, err error) {
+	n, err = i.Reader.Read(p)
+	i.setBar(n)
+	return
+}
+
+func (i *ProgressIndicator) setBar(n int) {
+	i.count += float64(n)
+
+	if i.bar != nil {
+		_ = i.bar.Set((int)(i.count * 100 / i.Total))
+	}
+}
